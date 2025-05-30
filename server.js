734721528df5d466a3eaea3b38fe103b789d7733
Kitonyi <162415030+DevZoeKitonyi@users.noreply.github.com>
2025-05-30 16:40:03 +0300

    Update server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+// Imports
+const express = require('express');
+const mongoose = require('mongoose');
+const taskRoutes = require('./routes')
+
+// Create Express server & middleware
+const app = express();
+app.use(express.json());
+
+// Constants
+const mongoUri = 'mongodb://localhost:27017/taskdb';
+const PORT = 3000;
+
+// MongoDB Connection
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=> console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDb connection error:', err));
+
+
+//Use task routes
+app.use('/', taskRoutes);
+
+
+//Fire up the server
+app.listen(PORT, ()=> {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
